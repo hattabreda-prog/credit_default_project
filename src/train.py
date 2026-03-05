9cc16ccfe2bb6f5f65dc9a9457c846d1acd4613d
hattabreda-prog <hattab.reda@gmail.com>
2026-03-05 12:52:41 +0100

    feat: random forest model with recall@topK metric

--- src/train.py
+++ src/train.py
@@ -1,16 +1,23 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import average_precision_score
+from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 from data_prep import prepare_data
 from config import MODEL_PATH
-
+from metrics import recall_at_topk
+from config import TOPK
 
 def train_model():
 
     X_train, X_test, y_train, y_test = prepare_data()
 
-    model = LogisticRegression(class_weight="balanced", max_iter=1000)
+    model = RandomForestClassifier(
+    n_estimators=300,
+    max_depth=8,
+    class_weight="balanced",
+    random_state=42
+)
 
     model.fit(X_train, y_train)
 
@@ -19,6 +26,9 @@ def train_model():
     pr_auc = average_precision_score(y_test, y_proba)
 
     print("PR-AUC :", pr_auc)
+    recall_topk = recall_at_topk(y_test, y_proba, TOPK)
+
+    print("Recall@TopK :", recall_topk)
 
     joblib.dump(model, MODEL_PATH)
 
